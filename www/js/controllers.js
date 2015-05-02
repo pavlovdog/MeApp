34253904cc517093a25e24800d63d19eb4890e90
pavlovdog <sergeydolg2@gmail.com>
2015-05-02 21:51:01 +0300

    Great work! Me is ready, without edit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,48 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope,$ionicPopup) {
 	$scope.contacts=[
-	{
-		resourse:'Twitter',
-		name:'@pavlovdog',
-		image:'icon ion-social-twitter-outline'
+	{	
+		resourse: 'Twitter',
+		name: '@pavlovdog',
+		image: 'icon ion-social-twitter-outline'
 	},{
-		resourse:'Facebook',
-		name:'facebook.pavlovdog.com',
-		image:'icon ion-social-facebook-outline'
+		resourse: 'Facebook',
+		name: 'facebook.pavlovdog.com',
+		image: 'icon ion-social-facebook-outline'
 	},{
-		resourse:'Github',
-		name:'pavlovdog@github.com',
-		image:'icon ion-social-github-outline'
+		resourse: 'Github',
+		name: 'pavlovdog@github.com',
+		image: 'icon ion-social-github-outline'
 	},{
-		resourse:'Instagram',
-		name:'#pavlovdog',
-		image:'icon ion-social-instagram-outline'
+		resourse: 'Instagram',
+		name: '#pavlovdog',
+		image: 'icon ion-social-instagram-outline'
 	},{
-		resourse:'Phone number',
-		name:'#pavlovdog',
-		image:'icon ion-social-instagram-outline'
+		resourse: 'Phone number',
+		name: '8-(916)-123-23-57',
+		image: 'icon ion-ios-telephone-outline'
 	}];
+
+	$scope.ShowConfirm = function(contact) {
+		var confirmPopup = $ionicPopup.confirm({
+			title: 'Are you sure?',
+			// template: 'Are you sure you want to eat this ice cream?'
+			cancelText: 'No',
+			okText: 'Yes'     
+		});
+		confirmPopup.then(function(res) {
+			if(res) {
+				$scope.contacts.splice($scope.contacts.indexOf(contact),1);
+			} else {
+				console.log('Nope');
+			}
+		});
+	};
+
+	$scope.ShowEdit = function(){
+
+	}
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
