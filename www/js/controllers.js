8b2415848ac7c9b1e26221a235c4bc3439bbde3a
pavlovdog <sergeydolg2@gmail.com>
2015-05-03 13:50:49 +0300

    Big dict, error edit, error fb

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,87 +1,90 @@
-angular.module('starter.controllers', ['ionic'])
+angular.module('starter.controllers', ['ionic','firebase'])
 
-.controller('DashCtrl', function($scope,$ionicPopup,$timeout) {
-	$scope.contacts=[
-	{	
-		id: 0,
-		resourse: 'Twitter',
-		name: '@pavlovdog',
-		image: 'icon ion-social-twitter-outline'
-	},{
-		id: 1,
-		resourse: 'Facebook',
-		name: 'facebook.pavlovdog.com',
-		image: 'icon ion-social-facebook-outline'
-	},{
-		id: 2,
-		resourse: 'Github',
-		name: 'pavlovdog@github.com',
-		image: 'icon ion-social-github-outline'
-	},{
-		id: 3,
-		resourse: 'Instagram',
-		name: '#pavlovdog',
-		image: 'icon ion-social-instagram-outline'
-	},{
-		id: 4,
-		resourse: 'Phone number',
-		name: '8-(916)-123-23-57',
-		image: 'icon ion-ios-telephone-outline'
-	}];
+.controller('DashCtrl', ['$scope','$ionicPopup','$timeout','$firebaseObject',
+	function($scope,$ionicPopup,$timeout,$firebaseObject) {
+		// var ref = new Firebase("https://meappionic.firebaseio.com/");
 
-	$scope.ShowConfirm = function(contact) {
-		console.log(contact);
-		var confirmPopup = $ionicPopup.confirm({
-			title: 'Are you sure?',
-			cancelText: 'No',
-			okText: 'Yes'     
-		});
-		confirmPopup.then(function(res) {
-			if(res) {
-				$scope.contacts.splice($scope.contacts.indexOf(contact),1);
-			} else {
-				console.log('Nope');
-			}
-		});
-	};
+		// // download the data into a local object
+		// $scope.FB = $firebaseObject(ref);
+		// console.log($scope.FB);
 
-	$scope.ShowEdit = function(contact) {
-
-		$scope.newLogin='';
-		// An elaborate, custom popup
-		var myPopup = $ionicPopup.show({
-		template: '<input type="text" ng-model="newLogin">',
-		title: 'Enter your login',
-		// subTitle: 'Please use normal things',
-		scope: $scope,
-		buttons: 
-		[{ text: 'Cancel' },
-		{
-			text: '<b>Save</b>',
-			type: 'button-positive',
-			onTap: function(e) {
-				if (!$scope.newLogin) {
-					//don't allow the user to close unless he enters smth
-					e.preventDefault();
+		// List of contacts
+		// Don't delete after firebase adding! 
+		$scope.contacts=
+		{pavlovdog:[{	
+						id: 0,
+						resourse: 'Twitter',
+						name: '@pavlovdog',
+						image: 'icon ion-social-twitter-outline'
+					},{
+						id: 1,
+						resourse: 'Facebook',
+						name: 'facebook.pavlovdog.com',
+						image: 'icon ion-social-facebook-outline'
+					},{
+						id: 2,
+						resourse: 'Github',
+						name: 'pavlovdog@github.com',
+						image: 'icon ion-social-github-outline'
+					},{
+						id: 3,
+						resourse: 'Instagram',
+						name: '#pavlovdog',
+						image: 'icon ion-social-instagram-outline'
+					},{
+						id: 4,
+						resourse: 'Phone number',
+						name: '8-(916)-123-23-57',
+						image: 'icon ion-ios-telephone-outline'
+					}],
+		guest: 	    [{	
+						id: 0,
+						resourse: 'Twitter',
+						name: '@guest',
+						image: 'icon ion-social-twitter-outline'
+					},{
+						id: 1,
+						resourse: 'Facebook',
+						name: 'facebook.guest.com',
+						image: 'icon ion-social-facebook-outline'
+					},{
+						id: 2,
+						resourse: 'Github',
+						name: 'guest@github.com',
+						image: 'icon ion-social-github-outline'
+					},{
+						id: 3,
+						resourse: 'Instagram',
+						name: '#guest',
+						image: 'icon ion-social-instagram-outline'
+					},{
+						id: 4,
+						resourse: 'Phone number',
+						name: '8-(303)-924-23-57',
+						image: 'icon ion-ios-telephone-outline'
+					}]
+		};
+		console.log(JSON.stringify($scope.contacts));
+		// Confirm message to delete
+		$scope.ShowConfirm = function(contact) {
+			console.log(contact);
+			var confirmPopup = $ionicPopup.confirm({
+				title: 'Are you sure?',
+				cancelText: 'No',
+				okText: 'Yes'     
+			});
+			confirmPopup.then(function(res) {
+				if(res) {
+					$scope.contacts.splice($scope.contacts.indexOf(contact),1);
 				} else {
-					console.log($scope.newLogin);
-					return $scope.newLogin;
+					console.log('Nope');
 				}
-			}
-		}]
-		
-		});
-		myPopup.then(function(res) {
-			// console.log('Tapped!', res);
-			$scope.contacts[contact.id].name=res;
-		});
+			});};
 
-		// $timeout(function() {
-		// myPopup.close(); //close the popup after 3 seconds for some reason
-		// }, 3000);
-	};
-
-})
+		// Edit alert for contact, one day i'll fix it
+		$scope.ShowEdit = function(contact) {}
+	}
+])
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
@@ -91,6 +94,7 @@ angular.module('starter.controllers', ['ionic'])
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
